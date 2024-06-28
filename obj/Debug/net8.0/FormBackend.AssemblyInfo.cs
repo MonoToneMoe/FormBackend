e383c52dccc6769890da11ddc3b402f2e9f8d0c7
Aiden Foo <aidenfoo154@gmail.com>
2024-06-28 08:27:14 -0700

    soething

--- obj/Debug/net8.0/FormBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/FormBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c468cd2c1efee729cd409e306be844fabc07245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114bc88a098e6ae756d90663e40019d099166ec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
