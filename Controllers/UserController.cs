d3233761e6a2ac910e4b93752dff37782249ff3d
Aiden Foo <aidenfoo154@gmail.com>
2024-07-01 09:48:09 -0700

    adding get all users

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,6 +8,7 @@ namespace FormBackend.Controllers{
         private readonly UserService _service = userService;
         [HttpPost] [Route("AddUser")] public IActionResult AddUser([FromBody] CreateAccountDTO user) => _service.AddUser(user) ? Ok("sucessfully added") : BadRequest("error adding user");
         [HttpGet] [Route("Login")] public IActionResult Login([FromBody] LoginDTO login) => _service.Login(login) != null ? Ok() : BadRequest("error logging in");
+        [HttpGet] [Route("GetAllUsers")] public IEnumerable<UserDTO> GetAllUsers() => _service.GetUsers();
         [HttpPut] [Route("ResetPassword")] public IActionResult ResetPassword([FromBody] ResetPassDTO newPass) => _service.ResetPassword(newPass) != null ? Ok("password reset") : BadRequest("error resetting password");
         [HttpPut] [Route("EditUser")] public IActionResult EditUser([FromBody] UserModel UserToUpdate) => _service.EditUser(UserToUpdate) ? Ok("Successfully Updated") : BadRequest("Error updating user");
         [HttpDelete] [Route("DeleteUser/{id}")] public IActionResult DeleteUser(int id) => _service.DeleteUser(id) ? Ok("Successfully Deleted") : BadRequest("Error deleting user");
