6f8f201222ea70bafcf1364619bbdf5ffcfc1b79
Aiden Foo <aidenfoo154@gmail.com>
2024-07-02 09:41:11 -0700

    block code :(

--- Services/FormService.cs
+++ Services/FormService.cs
@@ -5,8 +5,23 @@ namespace FormBackend.Services{
         private readonly DataContext _context = context;
         public bool NewForm(FormModel newForm) => _context.FormInfo.Add(newForm) != null && _context.SaveChanges() != 0;
         public IEnumerable<FormModel> GetForms() => _context.FormInfo;
-        public bool EditForm(FormModel form) => _context.FormInfo.Update((FormModel)_context.FormInfo.Where(Oldform => Oldform.ID == form.ID)) != null && _context.SaveChanges() !=0;
-        public bool DeleteForm(int id) => _context.FormInfo.Remove((FormModel)_context.FormInfo.Where(form => form.ID == id)) != null && _context.SaveChanges() !=0;
+        public bool EditForm(FormModel form)
+        {
+            FormModel model = _context.FormInfo.SingleOrDefault(OldForm => OldForm.ID == form.ID);
+            if(model != null){
+                _context.FormInfo.Update(model);
+            }
+            return _context.SaveChanges() != 0;
+        }
+
+        public bool DeleteForm(int id){
+            FormModel model = _context.FormInfo.SingleOrDefault(OldForm => OldForm.ID == id);
+            if(model != null){
+                _context.FormInfo.Remove(model);
+            }
+            return _context.SaveChanges() != 0;
+        }
+
         public IEnumerable<FormModel> FilterByFirstName() => GetForms().OrderBy(item => item.FirstName);
         public IEnumerable<FormModel> FilterByLastName() => GetForms().OrderBy(item => item.LastName);
     }
--- obj/Release/net8.0/FormBackend.AssemblyInfo.cs
+++ obj/Release/net8.0/FormBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4514b351b520ac359c52e32202ba3242a915c3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df35a690a38bc771769a4188475dfdc6c38258f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
