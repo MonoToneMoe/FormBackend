ff8f74b8f46ee30c68279f6c486eb77867fc444b
Aiden Foo <aidenfoo154@gmail.com>
2024-07-02 08:24:33 -0700

    dlls again

--- obj/Release/net8.0/FormBackend.AssemblyInfo.cs
+++ obj/Release/net8.0/FormBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47eeb8a4b4d17357da80285636095ee5db73584")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+220d354c1a4a58ccf57f1e5623c872d432ab62c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
